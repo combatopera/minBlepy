eb8af8132cebf6409ab38ad4a84773b1c7c240bf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2018-03-01 21:26:47 +0000

    ren

--- mynblep/__init__.py
+++ mynblep/__init__.py
@@ -1,17 +1,17 @@
 # Copyright 2014 Andrzej Cichocki
 
-# This file is part of minblepy.
+# This file is part of mynblep.
 #
-# minblepy is free software: you can redistribute it and/or modify
+# mynblep is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# minblepy is distributed in the hope that it will be useful,
+# mynblep is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with minblepy.  If not, see <http://www.gnu.org/licenses/>.
+# along with mynblep.  If not, see <http://www.gnu.org/licenses/>.
 
