2143efb31fcd1455bb4acab357345e3990f9edde
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2018-03-02 00:48:53 +0000

    no such thing as long in py3

--- mynblep/minblep.py
+++ mynblep/minblep.py
@@ -39,15 +39,10 @@ class MinBleps:
       raise Exception("Expected scale %s but ideal is %s." % (scaleornone, idealscale))
     return idealscale
 
-  @staticmethod
-  def minrepr(n):
-    # Note int will return a long if the value is really too big for int:
-    return repr(int(n) if isinstance(n, long) else n)
-
   @classmethod
   def loadorcreate(cls, naiverate, outrate, scaleornone, cutoff = defaultcutoff, transition = defaulttransition):
     scale = cls.resolvescale(naiverate, outrate, scaleornone)
-    name = "%s(%s)" % (cls.__name__, ','.join(map(cls.minrepr, [naiverate, outrate, scale, cutoff, transition])))
+    name = "%s(%s)" % (cls.__name__, ','.join(map(repr, [naiverate, outrate, scale, cutoff, transition])))
     path = os.path.join(os.path.dirname(__file__), name)
     if os.path.exists(path):
       log.debug("Loading cached minBLEPs: %s", path)
