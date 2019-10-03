ed2c168796fb76d19fc2abca1188cf51dddbe41c
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-10-03 23:14:31 +0100

    snap

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ import setuptools
 
 setuptools.setup(
         name = 'mynblep',
-        version = '2',
+        version = '3-SNAPSHOT',
         install_requires = ['numpy', 'pyrbo'],
         packages = setuptools.find_packages())
