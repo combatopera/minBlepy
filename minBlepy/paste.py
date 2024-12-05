e1307feab025cca2215b2da485232535c82859ab
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-12-05 20:19:55 +0000

    problem was unroll clobbering mixinsize

--- minBlepy/paste.py
+++ minBlepy/paste.py
@@ -19,7 +19,7 @@ from . import u4
 from pyrbo import turbo
 import numpy as np
 
-@turbo(ampsize = u4, outp = [np.float32], naivex2outxp = [np.int32], outsize = u4, demultiplexedp = [np.float32], naivex2offp = [np.int32], ampp = [np.float32], naivex = u4, naiverate = u4, outrate = u4, mixinsize = u4, out0 = u4, dclevel = np.float32, dcindex = u4, ampchunk = u4, a = np.float32, i = u4, dccount = u4, mixinp = [np.float32])
+@turbo(ampsize = u4, outp = [np.float32], naivex2outxp = [np.int32], outsize = u4, demultiplexedp = [np.float32], naivex2offp = [np.int32], ampp = [np.float32], naivex = u4, naiverate = u4, outrate = u4, mixinsize = u4, out0 = u4, dclevel = np.float32, dcindex = u4, ampchunk = u4, a = np.float32, i = u4, k = u4, mixinp = [np.float32])
 def pasteminbleps(ampsize, outp, naivex2outxp, outsize, demultiplexedp, naivex2offp, ampp, naivex, naiverate, outrate, mixinsize):
     # TODO LATER: This code needs tests.
     out0 = naivex2outxp[naivex]
@@ -34,21 +34,21 @@ def pasteminbleps(ampsize, outp, naivex2outxp, outsize, demultiplexedp, naivex2o
                 i = naivex2outxp[naivex] - out0
                 mixinp = demultiplexedp + naivex2offp[naivex]
                 if dcindex <= i: # We can DC-adjust while pasting this mixin.
-                    dccount = i - dcindex
-                    for UNROLL in range(dccount):
+                    k = i - dcindex
+                    for UNROLL, CLOBBER in range(k):
                         outp[0] += dclevel
                         outp += 1
-                    for UNROLL in range(mixinsize):
+                    for UNROLL, k in range(mixinsize):
                         outp[0] += mixinp[0] * a + dclevel
                         outp += 1
                         mixinp += 1
                 else: # The mixin starts before the pending DC adjustment.
-                    dccount = i + mixinsize - dcindex
-                    for UNROLL in range(dccount):
+                    k = i + mixinsize - dcindex
+                    for UNROLL, CLOBBER in range(k):
                         outp[0] += dclevel
                         outp += 1
                     outp -= mixinsize
-                    for UNROLL in range(mixinsize):
+                    for UNROLL, k in range(mixinsize):
                         outp[0] += mixinp[0] * a
                         outp += 1
                         mixinp += 1
@@ -57,7 +57,7 @@ def pasteminbleps(ampsize, outp, naivex2outxp, outsize, demultiplexedp, naivex2o
         ampsize -= ampchunk
         naivex = 0
         out0 -= outrate
-    dccount = outsize - dcindex
-    for UNROLL in range(dccount):
+    k = outsize - dcindex
+    for UNROLL, CLOBBER in range(k):
         outp[0] += dclevel
         outp += 1
